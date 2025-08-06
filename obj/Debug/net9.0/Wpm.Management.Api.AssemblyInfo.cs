417a8ec5669c66b23236549f6c58b4427c417bb5
priyamilan.m@gmail.com <priyamilan.m@gmail.com>
2025-08-05 21:21:51 -0400

    blah

--- obj/Debug/net9.0/Wpm.Management.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/Wpm.Management.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wpm.Management.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df8551a7a1ab18374e843dabfefa564cbc3f4dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wpm.Management.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wpm.Management.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
